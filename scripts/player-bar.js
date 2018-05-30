cb206c121c222335c888231c8f5bd42607193136
Alex <alexpritchard45@gmail.com>
2018-05-30 14:15:01 -0400

    added total time

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,7 +1,12 @@
+//checkpointwork
+
 {
   $('button#play-pause').on('click', function() {
     player.playPause();
     $(this).attr('playState', player.playState);
+    //added total time
+    $('#time-control .total-time').text( player.currentlyPlaying.duration);
+
   });
 
   $('button#next').on('click', function(){
@@ -13,6 +18,25 @@
 
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+    //added total time
+    $('#time-control .total-time').text( nextSong.duration);
+  });
+//sliders
+  $('#time-control input').on('input', function (event){
+    player.skipTo(event.target.value);
+  });
+//sliders
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
+//assignment event handler
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
   });
   //assignment
     $('button#previous').on('click', function(){
@@ -25,7 +49,8 @@
 
       const previousSong = album.songs[previousSongIndex];
       player.playPause(previousSong);
-
+      //added total time
+      $('#time-control .total-time').text( previousSong.duration );
     });
 //both buttons work.
 }
